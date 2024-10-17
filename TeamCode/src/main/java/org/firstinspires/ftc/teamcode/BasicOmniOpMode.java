1c753bc682b53a7759e01babce50a5aae7d47671
Dreamer <91762617+DreamerCodes@users.noreply.github.com>
2024-10-17 16:14:13 -0500

    Updated BasicOmniOpMode.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicOmniOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicOmniOpMode.java
@@ -8,7 +8,7 @@ import com.qualcomm.robotcore.util.Range;
 
 @TeleOp(name="Basic: Omni Linear OpMode", group="Linear OpMode")
 //@Disabled
-public class BasicOmniOpMode_Linear extends LinearOpMode {
+public class BasicOmniOpMode extends LinearOpMode {
     // Declare OpMode members for each of the 4 motors.
     private ElapsedTime runtime = new ElapsedTime();
     private DcMotor leftFrontDrive = null;
