0b7923b1a35d4014456aef22687e46d070c30164
Dreamer <91762617+DreamerCodes@users.noreply.github.com>
2024-11-04 15:43:24 -0600

    Added OmniAutoOpMode.java [UNFINISHED]

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OmniAutoOpMode.java
@@ -0,0 +1,28 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+@TeleOp(name="Autonomous Omni Linear OpMode", group="Autonomous Opmode")
+public class OmniAutoOpMode {
+        // Declare OpMode members for each of the 4 motors.
+        private ElapsedTime runtime = new ElapsedTime();
+        private DcMotor leftFrontDrive = null;
+        private DcMotor leftBackDrive = null;
+        private DcMotor rightFrontDrive = null;
+        private DcMotor rightBackDrive = null;
+
+        private DcMotor armMotorRotate = null;
+        private DcMotor armExtend = null;
+
+        private Servo clawOpenClose;
+        private Servo clawRotate;
+        private Servo wrist;
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OmniOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OmniOpMode.java
@@ -28,7 +28,7 @@ wrist = "wrist"
 
 @TeleOp(name="Basic: Omni Linear OpMode", group="Linear OpMode")
 //@Disabled
-public class BasicOmniOpMode extends LinearOpMode {
+public class OmniOpMode extends LinearOpMode {
     // Declare OpMode members for each of the 4 motors.
     private ElapsedTime runtime = new ElapsedTime();
     private DcMotor leftFrontDrive = null;
